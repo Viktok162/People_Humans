a67e81254764e6b25c2c25ec8255d929ab2eac99
Viktok <viktok162@gmail.com>
2025-03-07 19:00:42 +0500

    Second edition

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,10 @@
 fun main() {
-    val likes: Int = 221
+    val likes: Int = 121 //количество лайков
     val str: String
-    if ((likes - 11) % 100 == 0 || likes % 100 == 0) {
-        println("Понравилось $likes человекам")
+    if ((likes - 11) % 100 == 0) {
+        println("Понравилось $likes людям")
     } else {
-        if ((likes - 1) % 10 == 0 || (likes - 11) % 100 == 0) str = "человеку" else str = "людям"
+        if ((likes - 1) % 10 == 0) str = "человеку" else str = "людям"
         println("Понравилось $likes $str")
     }
 }
